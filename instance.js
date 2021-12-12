78e3213a64162850b559c16308af36ecbf01457c
Lutvi Rosyady <moorails@gmail.com>
2021-12-12 17:15:44 +0700

    Updating Coins

--- instance.js
+++ instance.js
@@ -168,5 +168,174 @@ c.symbols = [
         stop_percent: 10.0
       }
     ]
+  },
+  {
+    symbol: 'CTKUSDT',
+    exchange: 'binance_futures',
+    periods: ['5m', '15m', '1h'],
+    trade: {
+      currency_capital: 100,
+      strategies: [
+        {
+          strategy: 'dca_dipper',
+          interval: '15m',
+          options: {
+            period: '15m'
+          }
+        }
+      ]
+    },
+    watchdogs: [
+      {
+        name: 'risk_reward_ratio',
+        target_percent: 3.0,
+        stop_percent: 10.0
+      }
+    ]
+  },
+  {
+    symbol: 'KEEPUSDT',
+    exchange: 'binance_futures',
+    periods: ['5m', '15m', '1h'],
+    trade: {
+      currency_capital: 100,
+      strategies: [
+        {
+          strategy: 'dca_dipper',
+          interval: '15m',
+          options: {
+            period: '15m'
+          }
+        }
+      ]
+    },
+    watchdogs: [
+      {
+        name: 'risk_reward_ratio',
+        target_percent: 3.0,
+        stop_percent: 10.0
+      }
+    ]
+  },
+  {
+    symbol: 'NEARUSDT',
+    exchange: 'binance_futures',
+    periods: ['5m', '15m', '1h'],
+    trade: {
+      currency_capital: 100,
+      strategies: [
+        {
+          strategy: 'dca_dipper',
+          interval: '15m',
+          options: {
+            period: '15m'
+          }
+        }
+      ]
+    },
+    watchdogs: [
+      {
+        name: 'risk_reward_ratio',
+        target_percent: 3.0,
+        stop_percent: 10.0
+      }
+    ]
+  },
+  {
+    symbol: 'ENJUSDT',
+    exchange: 'binance_futures',
+    periods: ['5m', '15m', '1h'],
+    trade: {
+      currency_capital: 100,
+      strategies: [
+        {
+          strategy: 'dca_dipper',
+          interval: '15m',
+          options: {
+            period: '15m'
+          }
+        }
+      ]
+    },
+    watchdogs: [
+      {
+        name: 'risk_reward_ratio',
+        target_percent: 3.0,
+        stop_percent: 10.0
+      }
+    ]
+  },
+  {
+    symbol: 'REEFUSDT',
+    exchange: 'binance_futures',
+    periods: ['5m', '15m', '1h'],
+    trade: {
+      currency_capital: 100,
+      strategies: [
+        {
+          strategy: 'dca_dipper',
+          interval: '15m',
+          options: {
+            period: '15m'
+          }
+        }
+      ]
+    },
+    watchdogs: [
+      {
+        name: 'risk_reward_ratio',
+        target_percent: 3.0,
+        stop_percent: 10.0
+      }
+    ]
+  },
+  {
+    symbol: '1000SHIBUSDT',
+    exchange: 'binance_futures',
+    periods: ['5m', '15m', '1h'],
+    trade: {
+      currency_capital: 100,
+      strategies: [
+        {
+          strategy: 'dca_dipper',
+          interval: '15m',
+          options: {
+            period: '15m'
+          }
+        }
+      ]
+    },
+    watchdogs: [
+      {
+        name: 'risk_reward_ratio',
+        target_percent: 3.0,
+        stop_percent: 10.0
+      }
+    ]
+  },
+  {
+    symbol: 'BAKEUSDT',
+    exchange: 'binance_futures',
+    periods: ['5m', '15m', '1h'],
+    trade: {
+      currency_capital: 100,
+      strategies: [
+        {
+          strategy: 'dca_dipper',
+          interval: '15m',
+          options: {
+            period: '15m'
+          }
+        }
+      ]
+    },
+    watchdogs: [
+      {
+        name: 'risk_reward_ratio',
+        target_percent: 3.0,
+        stop_percent: 10.0
+      }
+    ]
   }
 ];
+
